462f1254b907ca5e57c3b2a9bafb03cf55e9a049
Alessandro Paperini <alessandro.paperini@drwolf.it>
2021-03-10 16:46:25 +0100

    :truck: routes update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
+import { MessageDetailComponent } from './pages/message-detail/message-detail.component';
+import { MessagesComponent } from './pages/messages/messages.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: NavigationComponent
+    component: NavigationComponent,
+    children: [
+      {
+        path: '',
+        component: MessagesComponent
+      },
+      {
+        path: 'message/:id',
+        component: MessageDetailComponent
+      }
+    ]
   }
 ];
 
