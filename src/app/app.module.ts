318fdd200230e03b04b0f4c26a3d8ee5878a6502
Alessandro Paperini <alessandro.paperini@drwolf.it>
2021-03-10 15:13:25 +0100

    :construction: pages initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material.module';
 import { NavigationComponent } from './navigation/navigation.component';
+import { MessagesComponent } from './pages/messages/messages.component';
+import { MessageDetailComponent } from './pages/message-detail/message-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    MessagesComponent,
+    MessageDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/message-detail/message-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message-detail',
+  templateUrl: './message-detail.component.html',
+  styleUrls: ['./message-detail.component.scss']
+})
+export class MessageDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
