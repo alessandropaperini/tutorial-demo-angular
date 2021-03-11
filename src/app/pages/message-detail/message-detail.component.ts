27d6d588fd1f7d423c2949fe71e4a86f2710ca48
Alessandro Paperini <alessandro.paperini@drwolf.it>
2021-03-11 15:00:28 +0100

    :construction: message detail page update

--- src/app/pages/message-detail/message-detail.component.ts
+++ src/app/pages/message-detail/message-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { MESSAGES } from 'src/app/mock/mock-messages';
+import { Message } from 'src/app/model/message';
 
 @Component({
   selector: 'app-message-detail',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageDetailComponent implements OnInit {
 
-  constructor() { }
+  message: Message | undefined;
+
+  constructor(
+    private readonly route: ActivatedRoute
+  ) {
+    this.route.params
+      .pipe(
+        map(params => this.message = this.getMessage(+params.id))
+      )
+      .subscribe();
+  }
 
   ngOnInit(): void {
+  }
+
+  private getMessage(id: number): Message | undefined {
+    return MESSAGES.find(m => m.id === id);
   }
 
 }
