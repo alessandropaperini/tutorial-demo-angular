7d33a21d4303882ed6e75305a331eb3f9d1a3301
Alessandro Paperini <alessandro.paperini@drwolf.it>
2021-03-11 18:16:31 +0100

    :clown_face: mock messages update

--- src/app/mock/mock-messages.ts
+++ src/app/mock/mock-messages.ts
@@ -1,9 +1,9 @@
 import { Message } from "../model/message";
 
 export const MESSAGES: Message[] = [
-    { id: 1, title: 'Titolo messaggio 1', message: 'Questo è un messaggio di testo' },
-    { id: 2, title: 'Titolo messaggio 2', message: 'Questo è un messaggio di testo' },
-    { id: 3, title: 'Titolo messaggio 3', message: 'Questo è un messaggio di testo' },
-    { id: 4, title: 'Titolo messaggio 4', message: 'Questo è un messaggio di testo' },
-    { id: 5, title: 'Titolo messaggio 5', message: 'Questo è un messaggio di testo' }
+    { id: 1, title: 'Messaggio 1', message: 'Questo è un messaggio di testo' },
+    { id: 2, title: 'Messaggio 2', message: 'Questo è un messaggio di testo' },
+    { id: 3, title: 'Messaggio 3', message: 'Questo è un messaggio di testo' },
+    { id: 4, title: 'Messaggio 4', message: 'Questo è un messaggio di testo' },
+    { id: 5, title: 'Messaggio 5', message: 'Questo è un messaggio di testo' }
 ];
