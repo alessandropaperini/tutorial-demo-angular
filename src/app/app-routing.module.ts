e22ca6fefe673a3d87b935c323daf26f0099baf3
Alessandro Paperini <alessandro.paperini@drwolf.it>
2021-03-15 15:22:18 +0100

    :truck: routes update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
+import { LoginComponent } from './pages/login/login.component';
 import { MessageDetailComponent } from './pages/message-detail/message-detail.component';
 import { MessagesComponent } from './pages/messages/messages.component';
+import { AuthenticationGuard } from './services/guards/authentication.guard';
 
 const routes: Routes = [
   {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
     path: '',
     component: NavigationComponent,
+    canActivate: [AuthenticationGuard],
     children: [
       {
         path: '',
